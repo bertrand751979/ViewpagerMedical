fe05a50faf5caa8b0db7e7868fbcf797832b5166
bertrand751979 <bertrandassinou@gmail.com>
2022-02-05 21:00:38 +0100

    finalVersion

--- app/src/main/java/com/example/viewpagermedical/activities/MainActivity.java
+++ app/src/main/java/com/example/viewpagermedical/activities/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.viewpagermedical.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.viewpagermedical.R;
+import com.example.viewpagermedical.fragments.AboutFragment;
 import com.example.viewpagermedical.fragments.MainFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNav=findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+
         displayFragment();
     }
 
@@ -23,4 +33,27 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(R.id.fragment_container, new MainFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.nav_app:
+                    selectedFragment = new MainFragment();
+                    break;
+            }
+
+            switch (item.getItemId()) {
+                case R.id.nav_about:
+                    selectedFragment = new AboutFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
+
+
+
 }
